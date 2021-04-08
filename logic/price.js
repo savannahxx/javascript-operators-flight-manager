46e65a12ee2da7bfa8f78852d54f7146d14a4658
Savannah Martin <savannnahh@outlook.com>
2021-04-08 11:58:38 -0400

    Module 2

--- logic/price.js
+++ logic/price.js
@@ -1,4 +1,38 @@
 function Prices() {
+    function calculateFinalPrice(basePrice, variationPassenger, variationFlight) {
+        let finalPrice = (basePrice + (variationPassenger / 100) * basePrice) 
+                        + (variationFlight / 100) * (basePrice + (variationPassenger/100) * 100);
+        
+        return finalPrice.toFixed(2);
+    }
 
+    function calculateDefaultFinalPrice(basePrice, passengerType, flightType) {
+        if (passengerType.toLowerCase() === "regular"
+            && flightType.toLowerCase() === "economy") {
+            basePrice = basePrice - basePrice * .05 - .03 * (basePrice - .05 * basePrice);
+        }
+        else if (passengerType.toLowerCase() === "regular"
+                && flightType.toLowerCase() === "business") {
+            basePrice = basePrice - basePrice * .05 + .10 * (basePrice - .05 * basePrice);
+        }
+        else if (passengerType.toLowerCase() === "vip"
+                && flightType.toLowerCase() === "economy") {
+            basePrice = basePrice + basePrice * .05 - .03 * (basePrice + .05 * basePrice);
+        }
+        else if (passengerType.toLowerCase() === "vip"
+                && flightType.toLowerCase() === "business") {
+            basePrice = basePrice + basePrice * .05 + .10 * (basePrice + .05 * basePrice);
+        }
+
+        return basePrice;
+    }
+
+    function calculateTotalFinalPrice(numSeats, passengerType, flightType, basePrice) {
+        return numSeats * calculateDefaultFinalPrice(basePrice, passengerType, flightType);
+    }
+
+    return { calculateFinalPrice, calculateDefaultFinalPrice, calculateTotalFinalPrice };
 }
+
+module.exports = Prices();
 
--- logic/util.js
+++ logic/util.js
@@ -14,9 +14,31 @@ function Util() {
       sumPassengers += totalPassengers[i];
     }
     return sumPassengers;
- }
+  }
 
-  return { calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers };
+  function checkInput(input) {
+    if (input.isEmpty() || Number.isNaN(input)) {
+      throw new Error();
+    }
+  }
+ 
+  function calculateTotalDistance(distances) {
+    let totalDistance = 0;
+    for (i = 0; i < distances.length; i++) {
+      if (distances[i] > 0) {
+        totalDistance += distances[i];
+      }
+    }
+    return totalDistance;
+  }
+
+  function calculateBonusPoints(businessDistances, economyDistances, businessBonus, economyBonus) {
+    let businessPoints = businessBonus / 100 * calculateTotalDistance(businessDistances);
+    let economyPoints = economyBonus / 100 * calculateTotalDistance(economyDistances);
+    return businessPoints + economyPoints;
+  }
+
+  return { calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers, checkInput, calculateTotalDistance, calculateBonusPoints };
 }
 
 module.exports = Util();
